6fd5e11e6a6fbf6927d82e872169a7baaa2aeaab
Marco Congedo <marco.congedo@gmail.com>
2026-06-10 14:30:11 +0200

    dev

--- src/extras/LOCAL_BUILD.jl
+++ src/extras/LOCAL_BUILD.jl
@@ -1,14 +1,16 @@
-#   This script is not part of the Loreat.jl package.
+#   This script is not part of the Xloreta.jl package.
 #   It allows to build the package locally from the source code,
 #   without actually installing the package.
 #   You won't need this script for using the package.
+#   NB: YOU MUST USE JULIA V1.12.4 OR MUST DELETE THE MANIFEST.TOML FILE AND 
+#   REACTIVATE THE ENVIRONMENT WITH: using Pkg; Pkg.activate("."); Pkg.instantiate()
 #
 #   MIT License
 #   Copyright (c) 2026, Marco Congedo, CNRS, Grenobe, France:
 #   https://sites.google.com/site/marcocongedo/home
 #
 #   DIRECTIONS:
-#   1) If you have installed the Loreta.jl from github or Julia registry, uninstall it.
+#   1) If you have installed the Xloreta.jl from github or Julia registry, uninstall it.
 #   3) Run this block (With VS code, click anywhere here and hit ALT+Enter)
 #
 begin
